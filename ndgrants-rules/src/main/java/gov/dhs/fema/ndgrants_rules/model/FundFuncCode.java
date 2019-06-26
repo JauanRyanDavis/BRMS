40644c3072a85a122d510be603c097586d50d6d7
workbench <>
2019-06-26 11:06:58 -0400

    init save for FundFuncCode {/ndgrants-rules/src/main/java/gov/dhs/fema/ndgrants_rules/model/FundFuncCode.java}

--- ndgrants-rules/src/main/java/gov/dhs/fema/ndgrants_rules/model/FundFuncCode.java
+++ ndgrants-rules/src/main/java/gov/dhs/fema/ndgrants_rules/model/FundFuncCode.java
@@ -7,8 +7,15 @@ package gov.dhs.fema.ndgrants_rules.model;
 public class FundFuncCode implements java.io.Serializable {
 
     static final long serialVersionUID = 1L;
+    
+    public static final FundFuncCode D = new FundFuncCode(1);
+    public static final FundFuncCode R = new FundFuncCode(2);
 
     public FundFuncCode() {
+    }
+    
+    FundFuncCode(long id){
+        super(id);
     }
 
 }
